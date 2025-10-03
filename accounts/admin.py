81e8bfac16432518ceb80510c70f1022eda051f7
juan quintero <codelatincolombia@gmail.com>
2025-10-02 23:43:55 -0500

    subiendo el repo2 ajustes superusers

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,44 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Auth
+# Register your models here.
 
 
 class AuthAdmin(UserAdmin):
-    # Campos que se mostrarán en la lista del admin pilass
-    list_display = ('email', 'username', 'name', 'lastname', 'date_joined', 'last_login', 'is_admin', 'is_staff', 'is_active')
-    
-    # Campos por los que se puede buscar devs ojo
-    search_fields = ('email', 'username', 'name', 'lastname')
-    
-    # Campos de solo lectura devs ojo
-    readonly_fields = ('date_joined', 'last_login')
-    
-    # Filtros laterales
-    list_filter = ('is_admin', 'is_staff', 'is_active', 'date_joined')
-    
-    # Ordenamiento por defecto ojito
-    ordering = ('-date_joined',)
-    
-    # Configuración de los campos en el formulario de edición
-    fieldsets = (
-        (None, {'fields': ('email', 'username', 'password')}),
-        ('Información personal', {'fields': ('name', 'lastname', 'phone_number')}),
-        ('Permisos', {'fields': ('is_active', 'is_staff', 'is_admin', 'is_superadmin', 'groups', 'user_permissions')}),
-        ('Fechas importantes', {'fields': ('last_login', 'date_joined')}),
-    )
-    
-    # Configuración para el formulario de creación de usuario
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('name', 'lastname', 'username', 'email', 'phone_number', 'password1', 'password2', 'is_active', 'is_staff', 'is_admin', 'is_superadmin'),
-        }),
-    )
+    list_display =('email','nombre','apellido','username','ultimo_login','fecha_registro','is_active',)
+    list_display_links =('email','nombre','apellido')
+    readonly_fields=('ultimo_login','fecha_registro')
+    ordering=('-fecha_registro',)
+    filter_horizontal=()
+    list_filter=()
+    fieldsets=()
 
-    # Especificar que el campo de identificación es 'email'
-    filter_horizontal = ('groups', 'user_permissions')
-    list_display_links = ('email', 'username')
-
-
-# Registrar el modelo personalizado en el admin
 admin.site.register(Auth, AuthAdmin)
+
+
+
--- accounts/models.py
+++ accounts/models.py
@@ -1,88 +1,56 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
-# Create your models here.
-class UserManager(BaseUserManager):
-    def create_user(self, name, lastname, username, email,  password=None, **extra_fields):
+
+class MiAdministradorCuentas(BaseUserManager):
+    def create_user(self, nombre, apellido, username, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('el usuario debe tener un email')
-        
+            raise ValueError("El usuario debe tener una dirección de correo electrónico")
         if not username:
-            raise ValueError('el usuario debe tener un username')
-        if not name:
-            raise ValueError('el usuario debe tener un nombre')
-        if not lastname:
-            raise ValueError('el usuario debe tener un apellido')
-        user = self.model(
-            name=name,
-            lastname=lastname,
-            username=username,
+            raise ValueError("El usuario debe tener un Nombre de Usuario")
+        
+        usuario = self.model(
             email=self.normalize_email(email),
+            username=username,
+            nombre=nombre,
+            apellido=apellido,
             **extra_fields
         )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, name, lastname, username, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_admin', True) # Superuser debe estar activo
-        extra_fields.setdefault('is_staff', True) # Superuser must have is_staff=True esta activo
-        extra_fields.setdefault('is_superadmin', True)# Superuser must have is_superuser=True # tiene todos los permisos admin
-
-        
-
-        return self.create_user(name, lastname, username, email, password, **extra_fields)
+        usuario.set_password(password)
+        usuario.save(using=self._db)
+        return usuario
     
-'''class Auth(AbstractBaseUser, PermissionsMixin):'''
+    def create_superuser(self, nombre, apellido, username, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_admin", True)
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superadmin", True)
 
-class Auth(AbstractBaseUser, PermissionsMixin):
-    name = models.CharField(max_length=50)
-    lastname = models.CharField(max_length=50)
-    username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(max_length=100, unique=True)
-    phone_number = models.CharField(max_length=12, blank=True)
+        return self.create_user(
+            nombre=nombre, apellido=apellido, username=username, email=email, password=password, **extra_fields
+        )
 
-    
-    date_joined = models.DateTimeField(auto_now_add=True)
-    last_login = models.DateTimeField(auto_now=True)
+
+
+class Auth(AbstractBaseUser, PermissionsMixin):  # Agrega PermissionsMixin
+    nombre = models.CharField(max_length=50)
+    apellido= models.CharField(max_length=50)
+    username= models.CharField(max_length=50, unique=True)
+    email= models.EmailField(max_length=100, unique=True)
+    telefono = models.CharField(max_length=15, blank=True, null=True)
+
+
+    # requeridos
+    fecha_registro = models.DateTimeField(auto_now_add=True)
+    ultimo_login = models.DateTimeField(auto_now=True,blank=True,)
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True) 
+    is_active = models.BooleanField(default=True)  # Cambia a True para permitir inicios de sesión
     is_superadmin = models.BooleanField(default=False)
-    
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'name', 'lastname']
+    REQUIRED_FIELDS = ['username', 'nombre', 'apellido']
 
-    objects = UserManager()
-
-    def __str__(self):
-        return self.email
-    
-    def has_perm(self, perm, obj=None):
-        return self.is_superuser
-
-    def has_module_perms(self, app_label):
-        return self.is_superuser
-    name = models.CharField(max_length=50)
-    lastname = models.CharField(max_length=50)
-    username = models.CharField(max_length=50, unique=True)
-    email = models.EmailField(max_length=100, unique=True)
-    phone_number = models.CharField(max_length=12, blank=True)
-
-    # required
-    date_joined = models.DateTimeField(auto_now_add=True)
-    last_login = models.DateTimeField(auto_now=True, null=True, blank=True)
-
-    is_admin = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
-    is_superadmin = models.BooleanField(default=False)
-
-    USERNAME_FIELD = 'email' # para iniciar sesion
-    REQUIRED_FIELDS = ['username', 'name', 'lastname'] # campos obligatorios
-
-    objects = UserManager()
+    objects = MiAdministradorCuentas()
 
     def __str__(self):
         return self.email
